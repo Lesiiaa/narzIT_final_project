08b493372d5e3ba888bdfebd99a6c11d306fc8ca
Letycja <letycja.niemiec@gmail.com>
2024-05-13 19:37:25 +0200

    Task3

--- main.py
+++ main.py
@@ -11,6 +11,13 @@ def load_file(filename):
         print("Error: Wrong file format!")
     return data
 
+def save_file(filename, data):
+    file_extension = filename.split('.')[-1]
+    if file_extension == 'json':
+        save_json(filename, data)
+    else:
+        print("Error: Wrong file format!")
+
 
 def load_json(filename):
     with open(filename, 'r') as file:
@@ -22,6 +29,13 @@ def load_json(filename):
             print("Error: Invalid JSON syntax!")
             return {}
 
+
+def save_json(filename, data):
+    with open(filename, 'w') as file:
+        json.dump(data, file, indent=4)
+    print("Json saved!")
+
+
 def main():
     parser = argparse.ArgumentParser(description="Data Processor")
     parser.add_argument("input_files", nargs=2, help="Input files + formats")
@@ -31,6 +45,9 @@ def main():
 
     filename, file_format = input_files[0].split('.')
     data = load_file(filename + '.' + file_format)
+
+    output_file = input_files[1]
+    save_file(output_file, data)
 
 if __name__ == "__main__":
     main()
